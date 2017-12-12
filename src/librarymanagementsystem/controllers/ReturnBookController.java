58c02b9076ae11821737b6a70ced4f81bc65c27e
Kevin Florenz T. Daus <kevin@optidig.biz>
2017-12-13 05:39:04 +0800

    ADD = return book controller

--- src/librarymanagementsystem/controllers/ReturnBookController.java
+++ src/librarymanagementsystem/controllers/ReturnBookController.java
@@ -5,9 +5,13 @@
  */
 package librarymanagementsystem.controllers;
 
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -16,12 +20,55 @@ import javafx.fxml.Initializable;
  */
 public class ReturnBookController implements Initializable {
 
+    @FXML
+    private Text borrowerName;
+
+    @FXML
+    private Text borrowerContactInformation;
+
+    @FXML
+    private Text borrowerAddress;
+
+    @FXML
+    private Text overdueTime;
+
+    @FXML
+    private Text penaltyPerDay;
+
+    @FXML
+    private Text dateToday;
+
+    @FXML
+    private Text totalPenalty;
+
+    @FXML
+    private JFXTextField bookIdScanned;
+
+    @FXML
+    private Text bookTitle;
+
+    @FXML
+    private Text bookEdition;
+
+    @FXML
+    private Text bookISBN;
+
+    @FXML
+    void returnBack(ActionEvent event) {
+        librarymanagementsystem.LibraryManagementSystem.back();
+    }
+
+    @FXML
+    void returnBook(ActionEvent event) {
+        librarymanagementsystem.LibraryManagementSystem.back();
+    }
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
