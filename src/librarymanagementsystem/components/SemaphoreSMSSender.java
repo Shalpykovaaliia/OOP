e2047ed853cd0529c89b5f9b0904127dd7b27eae
Kevin Florenz T. Daus <kevin@optidig.biz>
2018-01-04 03:54:45 +0800

    CHANGE = update the server url to use api.semaphone.com

--- src/librarymanagementsystem/components/SemaphoreSMSSender.java
+++ src/librarymanagementsystem/components/SemaphoreSMSSender.java
@@ -8,11 +8,6 @@ package librarymanagementsystem.components;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import librarymanagementsystem.LibraryManagementSystem;
@@ -35,7 +30,7 @@ public class SemaphoreSMSSender extends BaseSMSSender implements SmsSenderInterf
             String apiCode = LibraryManagementSystem.API_CODE;
             String smsSender = LibraryManagementSystem.SMS_SENDER_NAME;
             
-            HttpResponse< String> result = Unirest.post("https://api.ipify.org")
+            HttpResponse< String> result = Unirest.post("http://api.semaphore.co/api/v4/messages")
                     .field("apikey", apiCode)
                     .field("number", getRecipient())
                     .field("message", getMessage())
