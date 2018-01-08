2f393acdbb5e0017cbb6f1b003b1bb8cee5369ac
Kevin Florenz T. Daus <kevin@optidig.biz>
2018-01-09 06:05:11 +0800

    ADD = a utility class that retrieves application settings

--- src/librarymanagementsystem/components/SettingsRetriever.java
+++ src/librarymanagementsystem/components/SettingsRetriever.java
@@ -5,21 +5,70 @@
  */
 package librarymanagementsystem.components;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import librarymanagementsystem.facade.SettingFacade;
+import librarymanagementsystem.models.Setting;
+
 /**
  *
  * @author User
  */
 public class SettingsRetriever {
-    public static String getApiCode(){
 
-        return "";
-    }
-    public static Boolean isSmsNotificationEnable(){
-        
-        return true;
+    private EntityManager em;
+    private EntityManagerFactory emf;
+    protected SettingFacade settingsFacade;
+
+    public SettingsRetriever() {
+        this.emf = librarymanagementsystem.LibraryManagementSystem.APP_ENTITY_MANAGER_FACTORY;
+        this.em = emf.createEntityManager();
+        this.settingsFacade = new SettingFacade(emf);
     }
 
-    public static String getSmsSenderName() {   
-        return "";
+    public String getApiCode() {
+        TypedQuery<Setting> query = em.createNamedQuery("Setting.smsApiCode", Setting.class);
+        Setting returnedSetting = null;
+        try {
+            returnedSetting = query.getSingleResult();
+            return returnedSetting.getSettingValue();
+        } catch (javax.persistence.NoResultException ex) {
+            // create the setting , default to enabled
+            Setting notificationStatus = new Setting();
+            notificationStatus.setSettingName("SMS_API_CODE");
+            notificationStatus.setSettingValue("");
+            this.settingsFacade.create(notificationStatus);
+            return notificationStatus.getSettingValue();
+        }
+    }
+
+    public String getSmsSenderName() {
+        TypedQuery<Setting> query = em.createNamedQuery("Setting.smsSenderName", Setting.class);
+        try {
+            Setting returnedSetting = query.getSingleResult();
+            return returnedSetting.getSettingValue();
+        } catch (javax.persistence.NoResultException ex) {
+            Setting notificationStatus = new Setting();
+            notificationStatus.setSettingName("SMS_SENDER_NAME");
+            notificationStatus.setSettingValue("SEMAPHORE");
+            this.settingsFacade.create(notificationStatus);
+            return notificationStatus.getSettingValue();
+        }
+    }
+
+    public String getNotificationStatus() {
+        TypedQuery<Setting> query = em.createNamedQuery("Setting.notificationStatus", Setting.class);
+        Setting returnedSetting = null;
+        try {
+            returnedSetting = query.getSingleResult();
+            return returnedSetting.getSettingValue();
+        } catch (javax.persistence.NoResultException ex) {
+            Setting notificationStatus = new Setting();
+            notificationStatus.setSettingName("NOTIFICATION_STATUS");
+            notificationStatus.setSettingValue("enabled");
+            this.settingsFacade.create(notificationStatus);
+            return notificationStatus.getSettingValue();
+        }
     }
 }
