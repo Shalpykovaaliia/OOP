bb80d1af397407bce9077e7ae971e7dc0ca320d9
Kevin Florenz T. Daus <kevin@optidig.biz>
2018-03-01 05:32:49 +0800

    added pop over and information on dashboard

--- src/librarymanagementsystem/controllers/DashboardController.java
+++ src/librarymanagementsystem/controllers/DashboardController.java
@@ -9,20 +9,27 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.StageStyle;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
@@ -30,10 +37,10 @@ import librarymanagementsystem.LibraryManagementSystem;
 import librarymanagementsystem.facade.BooksFacade;
 import librarymanagementsystem.facade.BorrowerJpaController;
 import librarymanagementsystem.models.BookBorrower;
-import librarymanagementsystem.models.BookOverdue;
 import librarymanagementsystem.models.Books;
 import librarymanagementsystem.models.Borrower;
 import librarymanagementsystem.models.User;
+import org.controlsfx.control.PopOver;
 import org.eclipse.persistence.config.HintValues;
 import org.eclipse.persistence.config.QueryHints;
 
@@ -94,6 +101,13 @@ public class DashboardController implements Initializable {
 
     private BooksFacade booksFacade;
 
+    protected HashMap<Books, Borrower> bookToBorrowerMap;
+
+    protected HashMap<Borrower, List<Books>> borrowerToBook;
+    private ContextMenu borrowedBooksContextMenu;
+    private PopOver showBorrower;
+    private HashMap<Books, BookBorrower> overduedBooksMap;
+
     @FXML
     void logOut(ActionEvent event) {
         // close current view
@@ -141,6 +155,9 @@ public class DashboardController implements Initializable {
         this.loadDashboardRecords();
         // load over dued books
         this.loadOverDued();
+
+        showBorrower = new PopOver();
+
     }
 
     @FXML
@@ -183,8 +200,129 @@ public class DashboardController implements Initializable {
         LibraryManagementSystem.showView("book.penalty");
     }
 
+    @FXML
+    void showBorrowerOfBook(MouseEvent event) {
+        if (event.getClickCount() == 2) {
+            // @TODO
+            // get the selected borrowed books
+            String selectedBookTitle = this.borrowedBooksListView.getSelectionModel().getSelectedItem();
+            Set<Books> bookKeys = (Set<Books>) this.bookToBorrowerMap.keySet();
+            ArrayList<Books> bookList = new ArrayList<>(bookKeys);
+
+            Books filteredData;
+            // search the book by title
+            // get the bookborrower using book id
+            // open view
+            filteredData = bookList.stream()
+                    .filter(book -> book.getTitle().equals(selectedBookTitle))
+                    .findAny()
+                    .orElse(null);
+            Logger.getLogger(DashboardController.class.getName()).log(Level.INFO, filteredData.toString());
+            Borrower borrowerTempContainer = bookToBorrowerMap.get(filteredData);
+            VBox informationContainer = new VBox();
+            informationContainer.setSpacing(5);
+            informationContainer.setPadding(new Insets(10));
+            StringBuilder sb = new StringBuilder();
+            sb.append(borrowerTempContainer.getTitle())
+                    .append(" ")
+                    .append(borrowerTempContainer.getFirstname())
+                    .append(" ")
+                    .append(borrowerTempContainer.getLastname());
+            Text titleText = new Text(filteredData.getTitle());
+            titleText.setFont(new Font(20));
+            informationContainer.getChildren().add(titleText);
+            informationContainer.getChildren().add(new Text("Borrower : " + sb.toString()));
+            informationContainer.getChildren().add(new Text("Contact # : " + borrowerTempContainer.getMobileNumber()));
+            showBorrower.setTitle(" ");
+            showBorrower.setContentNode(informationContainer);
+            showBorrower.show(borrowedBooksListView, borrowedBooksListView.getScaleX() + borrowedBooksListView.getWidth() + 406, event.getSceneY() + 38);
+        }
+    }
+
+    @FXML
+    void showBorrowerdBookOfBorrower(MouseEvent event) {
+        if (event.getClickCount() == 2) {
+            // @TODO
+            // get the selected borrowed books
+            // search the name by title
+            // get the bookborrower using book id
+            // open view
+            String selectedFromActiveBorrower = activeBorrowersListView.getSelectionModel().getSelectedItem();
+            Set<Borrower> borrowerToBookKey = borrowerToBook.keySet();
+            ArrayList<Borrower> borrowerToBookKeyList = new ArrayList<>(borrowerToBookKey);
+            Borrower filteredData = borrowerToBookKeyList.stream()
+                    .filter(borrower -> (selectedFromActiveBorrower.equals(borrower.getFirstname() + " " + borrower.getLastname())))
+                    .findAny()
+                    .orElse(null);
+            Logger.getLogger(DashboardController.class.getName()).log(Level.INFO, filteredData.toString());
+            List<Books> booksAssignedToBorrower = borrowerToBook.get(filteredData);
+            VBox informationContainer = new VBox();
+            informationContainer.setSpacing(5);
+            informationContainer.setPadding(new Insets(10));
+            StringBuilder sb = new StringBuilder();
+            sb.append(filteredData.getTitle())
+                    .append(" ")
+                    .append(filteredData.getFirstname())
+                    .append(" ")
+                    .append(filteredData.getLastname());
+            Text titleText = new Text(sb.toString());
+            titleText.setFont(new Font(20));
+            informationContainer.getChildren().add(titleText);
+            booksAssignedToBorrower.forEach((currentItem) -> {
+                informationContainer.getChildren().add(new Text(currentItem.getTitle()));
+            });
+            showBorrower.setTitle(" ");
+            showBorrower.setContentNode(informationContainer);
+            showBorrower.show(activeBorrowersListView, activeBorrowersListView.getScaleX() + activeBorrowersListView.getWidth() + 663, event.getSceneY() + 38);
+
+        }
+    }
+
+    @FXML
+    void showCurrentOverdueBookAndBorrower(MouseEvent event) {
+        if (event.getClickCount() == 2) {
+            //@TODO - 
+            // make the overdue available in class
+            String selectedOverdueBook = this.overdueBooksListView.getSelectionModel().getSelectedItem();
+            Set<Books> tempKeyContainer = this.overduedBooksMap.keySet();
+            Logger.getLogger(DashboardController.class.getName()).log(Level.INFO, tempKeyContainer.toString());
+            Logger.getLogger(DashboardController.class.getName()).log(Level.INFO, selectedOverdueBook);
+            Books filteredData = tempKeyContainer.stream()
+                    .filter(currentBook -> currentBook.getTitle().equals(selectedOverdueBook))
+                    .findAny()
+                    .orElse(null);
+            Logger.getLogger(DashboardController.class.getName()).log(Level.INFO, filteredData.toString());
+
+            BookBorrower tempBookBorrower = this.overduedBooksMap.get(filteredData);
+
+            Books tempBooksContainer = tempBookBorrower.getBook();
+            Borrower tempBorrowerContainer = tempBookBorrower.getBorrower();
+
+            VBox informationContainer = new VBox();
+            informationContainer.setSpacing(5);
+            informationContainer.setPadding(new Insets(10));
+
+            Text borrowerText = new Text("Borrower : " +tempBorrowerContainer.getTitle()+" "+tempBorrowerContainer.getFirstname()+" "+tempBorrowerContainer.getLastname());
+            Text bookText = new Text("Book : " +tempBooksContainer.getTitle());
+            Text computerFeeText = new Text("Penalty : P " +tempBookBorrower.getComputerFee() +".00" );
+            Text daysOverdueText = new Text("Overdue : " +tempBookBorrower.getOverDueDays()+ " day(s)");
+            informationContainer.getChildren().add(borrowerText);
+            informationContainer.getChildren().add(bookText);
+            informationContainer.getChildren().add(computerFeeText);
+            informationContainer.getChildren().add(daysOverdueText);
+
+            showBorrower.setTitle(" ");
+            showBorrower.setContentNode(informationContainer);
+            showBorrower.show(overdueBooksListView, overdueBooksListView.getScaleX() + overdueBooksListView.getWidth() + 920, event.getSceneY() + 38);
+
+            // on click , show the overdue information . and show how much to pay on that overdue record
+            // show the book and the and the borrower
+        }
+    }
+
     // @TODO - wrong implementation . use book borrower if date return is null and  expected return date is less date today
     private void loadOverDued() {
+        this.overduedBooksMap = new HashMap<>();
         TypedQuery<BookBorrower> query = em.createNamedQuery("BookBorrower.findOverduedBook", BookBorrower.class);
         query.setHint("javax.persistence.cache.storeMode", "REFRESH");
         query.setHint(QueryHints.REFRESH, HintValues.TRUE);
@@ -192,8 +330,9 @@ public class DashboardController implements Initializable {
         overdueBooksListView.getItems().clear();
         for (Iterator<BookBorrower> iterator = overduedBorrowedBooks.iterator(); iterator.hasNext();) {
             BookBorrower curOverduedBook = iterator.next();
-            Books curBook = this.booksFacade.findBooks(curOverduedBook.getBookId());
+            Books curBook = curOverduedBook.getBook();
             overdueBooksListView.getItems().add(curBook.getTitle());
+            this.overduedBooksMap.put(curBook, curOverduedBook);
         }
     }
 
@@ -201,19 +340,35 @@ public class DashboardController implements Initializable {
         // load borrowed books
         TypedQuery<BookBorrower> query = em.createNamedQuery("BookBorrower.findUnique", BookBorrower.class);
         List<BookBorrower> result = query.getResultList();
+        bookToBorrowerMap = new HashMap<>();
+        borrowerToBook = new HashMap<>();
+
         HashSet<String> activeBooksBorrowed = new HashSet<>();
         HashSet<String> activeBorrowersName = new HashSet<>();
         for (Iterator<BookBorrower> iterator = result.iterator(); iterator.hasNext();) {
             BookBorrower next = iterator.next();
-            Borrower borrower = next.getBorrowerId();
-            Books foundBook = booksFacade.findBooks(next.getBookId());
+            Borrower borrower = next.getBorrower();
+            Books foundBook = next.getBook();
             StringBuilder borrowersName = new StringBuilder();
             borrowersName.append(borrower.getFirstname())
                     .append(" ")
                     .append(borrower.getLastname());
             activeBooksBorrowed.add(foundBook.getTitle());
             activeBorrowersName.add(borrowersName.toString());
-            Logger.getLogger(DashboardController.class.getName()).log(Level.INFO, "Book " + next.getBookId());
+            //Register the borrower to books map
+            if (bookToBorrowerMap.get(foundBook) == null) {
+                bookToBorrowerMap.put(foundBook, borrower);
+            }
+            // Register the books to borrower map
+            if (borrowerToBook.get(borrower) != null) {
+                borrowerToBook.get(borrower).add(foundBook);
+            } else {
+                ArrayList<Books> initialBook = new ArrayList<>();
+                initialBook.add(foundBook);
+                borrowerToBook.put(borrower, initialBook);
+            }
+
+            Logger.getLogger(DashboardController.class.getName()).log(Level.INFO, "Book " + next.getBook());
         }
         // From Set , load the items to the ListView
         borrowedBooksListView.getItems().clear();
